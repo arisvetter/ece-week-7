c520c9a11c2377b11c93f6723c369c4ed30caed7
arisvetter <142842842+arisvetter@users.noreply.github.com>
2025-10-07 16:07:40 +0000

    wires

--- class (2) 2/top.v
+++ class (2) 2/top.v
@@ -1,27 +1,37 @@
 
 
 
-wire carryout_in;
+
+
+wire [3:0] APlusB;
+wire carry_0;
+wire carry_1;
+wire carry_2;
+wire carry_3;
+wire around;
+
+
+
 ones_complement ones_complement_inst_1 (
     .A(sw[4]),
     .B(sw[6]),
     .carry(0),
     .Y(led[3]),
-    .Cout(carryout_in)
+    .Cout(carry_0)
 );
 
 ones_complement ones_complement_inst_2 (
     .A(sw[5]),
     .B(sw[7]),
-    .Cin(carryout_in),
+    .Cin(carry_0),
     .Y(led[4]),
-    .Cout(led[5])
+    .Cout(carry_1)
 );
 
 ones_complement ones_complement_inst_3 (
     .A(sw[4]),
     .B(sw[6]),
-    .carry(0),
+    .carry(carry_1),
     .Y(led[3]),
     .Cout(carryout_in)
 );
